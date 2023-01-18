64cb55ac1f27e218a24242a9f88be8d1d3510aae
Uncensored <Uncensored@DESKTOP-SVLGO47>
2023-01-18 14:44:59 +0200

    add dependency factory and remove unnecessary comments

---
+++ TaxCalculator.API/Factories/DependencyFactory.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using TaxCalculator.API.DAL;
+using TaxCalculator.API.DAL.Repositories;
+using TaxCalculator.API.DAL.Validators;
+using TaxCalculator.API.Models;
+using TaxCalculator.Common;
+
+namespace TaxCalculator.API.Factories
+{
+    public static class DependencyFactory
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddDateOnlyTimeOnlyStringConverters();
+            services.AddDbContext<TaxCalculatorDbContext>(opt => opt.UseInMemoryDatabase(Constants.DbSchemaName));
+            services.AddScoped<IValidator<TaxPayerModel>, TaxPayerValidator>();
+            services.AddScoped<ICalculatorRepository, CalculatorRepository>();
+        }
+    }
+}
--- TaxCalculator.API/Program.cs
+++ TaxCalculator.API/Program.cs
@@ -1,27 +1,16 @@
 using FluentValidation;
-using Microsoft.EntityFrameworkCore;
-using TaxCalculator.API.DAL;
 using TaxCalculator.API.DAL.Entities;
 using TaxCalculator.API.DAL.Repositories;
-using TaxCalculator.API.DAL.Validators;
 using TaxCalculator.API.Extensions;
+using TaxCalculator.API.Factories;
 using TaxCalculator.API.Models;
-using TaxCalculator.Common;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDateOnlyTimeOnlyStringConverters();
-builder.Services.AddDbContext<TaxCalculatorDbContext>(opt => opt.UseInMemoryDatabase(Constants.DbSchemaName));
-builder.Services.AddScoped<IValidator<TaxPayerModel>, TaxPayerValidator>();
-builder.Services.AddScoped<ICalculatorRepository, CalculatorRepository>();
+DependencyFactory.RegisterServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
